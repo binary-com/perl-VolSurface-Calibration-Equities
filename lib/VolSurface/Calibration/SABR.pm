7e71179ae6ee81b5091a4c67cae5596350026719
Mahdi Mohammadi <mm@binary.com>
2015-09-28 02:51:22 +0000

    removing references to DD

--- lib/VolSurface/Calibration/SABR.pm
+++ lib/VolSurface/Calibration/SABR.pm
@@ -20,7 +20,6 @@ use List::Util qw(min max);
 use Math::Trig qw(tanh);
 use Try::Tiny;
 use Format::Util::Numbers qw(roundnear);
-#use DataDog::DogStatsd::Helper qw(stats_gauge);
 
 our $VERSION = '0.01';
 
@@ -208,9 +207,6 @@ sub compute_parameterization {
         $new_values = $opt_using_initial_params;
     }
 
-    if ($self->price_with_parameterized_surface) {
-        #stats_gauge('index_vol_calibration_error ', $new_values->{calibration_error}, {tags => ['tag:' . $self->symbol]});
-    }
     # we expect the params to be passed in the correct order
     my %calib_params =
         pairwise { $a => $b } @{$self->calibration_param_names},
